4e7557eca756aa74cb252ca7e977d433a2ccadf4
Rudger Dame <rudger.dame@carvana.com>
2023-11-29 14:42:11 -0700

    update images

--- src/config.js
+++ src/config.js
@@ -1,10 +1,6 @@
 module.exports.SITE = {
-  name: 'TailNext',
-
-  origin: 'https://tailnext.vercel.app',
-  basePathname: '/',
-  trailingSlash: false,
-
-  title: 'TailNext — Your website with Next.js + Tailwind CSS',
+  name: 'Modulus Partners',
+  title: 'Modulus Partners',
   description: 'TailNext is a free and ready to start template to make your website using Next.js and Tailwind CSS.',
+  favicon: '/src/assets/images/MP_icon_shell.ico' // Add the path to your favicon here
 };
